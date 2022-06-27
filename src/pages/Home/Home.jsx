d7a8a5d62215c25a3d02ba0820148ad7ae9c8c9a
Aidaar <aidar21mamatov@gmail.com>
2022-06-27 20:49:00 +0600

    start project

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import About from '../../components/About/About'
+import Hero from '../../components/Hero/Hero'
+import Testimonials from '../../components/Testimonials/Testimonials'
+
+const Home = () => {
+	return (
+		<>
+			<Hero/>
+			<About />
+			<Testimonials/>
+		</>
+	)
+}
+
+export { Home }
